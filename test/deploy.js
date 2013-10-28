a5c97fd9290ecf1de48254fa948d42275d5148d5
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-10-29 00:47:32 +0100

    Remove npmjs.org test dependency

--- test/deploy.js
+++ test/deploy.js
@@ -4,11 +4,9 @@ var deploy = require('../lib/deploy');
 describe('deploy', function () {
   var options = {cwd: 'test/fixtures/'};
   var branch = 'testing';
-  var commit = 'dc8fcdf9eefb0dfd385286ee7e381ec4a1f4b288';
+  var commit = '941f066596a1f26d918b248baeb9a20a545903e6';
   var repoDir = options.cwd + 'repo/';
   var deployDir = options.cwd + 'www/';
-  // npm is sometimes rather slow
-  this.timeout(30000);
   before(function (done) {
     exec('tar zxf test/fixtures.tar.gz -C test', function (err) {
       if (err) return done(err);
